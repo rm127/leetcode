2287aed9fba4ff3f145853eccd8b8687fbdb9b6c
Rasmus <rasmus@mikkelstrup.dk>
2020-08-06 11:46:40 +0200

    Increased speed from 8ms to 4ms

--- 13-RomanToInteger.go
+++ 13-RomanToInteger.go
@@ -1,59 +1,37 @@
 package main
 
+var dict = map[byte]int{
+	'M': 1000,
+	'D': 500,
+	'C': 100,
+	'L': 50,
+	'X': 10,
+	'V': 5,
+	'I': 1,
+}
+
 func RomanToInteger(s string) int {
-	dict := map[rune]int{
-		'M': 1000,
-		'D': 500,
-		'C': 100,
-		'L': 50,
-		'X': 10,
-		'V': 5,
-		'I': 1,
+
+	if len(s) == 0 {
+		return 0
 	}
 
-	res := 0
+	length := len(s)
+	res := dict[s[length-1]]
 
-	for i := 0; i < len(s); i++ {
+	for i := length - 2; i >= 0; i-- {
 		curr := s[i]
+		next := s[i+1]
+		tempRes := dict[curr]
 
-		var next string
-		if i+1 < len(s) {
-			next = string(s[i+1])
+		if curr == 'I' && (next == 'V' || next == 'X') {
+			tempRes *= -1
+		} else if curr == 'X' && (next == 'L' || next == 'C') {
+			tempRes *= -1
+		} else if curr == 'C' && (next == 'D' || next == 'M') {
+			tempRes *= -1
 		}
-
-		match := false
-
-		if next != "" {
-			if curr == 'I' && next == "V" {
-				res += 4
-				match = true
-			}
-			if curr == 'I' && next == "X" {
-				res += 9
-				match = true
-			}
-			if curr == 'X' && next == "L" {
-				res += 40
-				match = true
-			}
-			if curr == 'X' && next == "C" {
-				res += 90
-				match = true
-			}
-			if curr == 'C' && next == "D" {
-				res += 400
-				match = true
-			}
-			if curr == 'C' && next == "M" {
-				res += 900
-				match = true
-			}
-		}
-		if match {
-			i++
-		} else {
-			res += dict[rune(curr)]
-		}
+		res += tempRes
 	}
 
 	return res
