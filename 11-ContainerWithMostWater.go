9719e168d95a79e5fdf31a4707602b3bd39b7665
Rasmus <rasmus@mikkelstrup.dk>
2020-08-07 09:15:58 +0200

    Added new problem (408ms runtime)

---
+++ 11-ContainerWithMostWater.go
@@ -0,0 +1,29 @@
+package main
+
+func ContainerWithMostWater(height []int) int {
+	max := 0
+
+	for i := 0; i < len(height); i++ {
+		for j := i + 1; j < len(height); j++ {
+			temp := Diff(i, j) * Min(height[i], height[j])
+			if temp > max {
+				max = temp
+			}
+		}
+	}
+	return max
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Diff(a, b int) int {
+	if a < b {
+		return b - a
+	}
+	return a - b
+}
---
+++ 11-ContainerWithMostWater_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestContainerWithMostWater(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example", args{[]int{1, 8, 6, 2, 5, 4, 8, 3, 7}}, 49},
+		{"Custom", args{[]int{1, 2, 3, 4, 5}}, 6},
+		{"Equal", args{[]int{3, 3, 3, 3}}, 9},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ContainerWithMostWater(tt.args.height); got != tt.want {
+				t.Errorf("ContainerWithMostWater() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
