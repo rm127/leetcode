fb3616346b44962c3719e2b098d17631a80b77e6
Rasmus <rasmus@mikkelstrup.dk>
2020-08-07 09:50:04 +0200

    Removed Diff function

--- 11-ContainerWithMostWater.go
+++ 11-ContainerWithMostWater.go
@@ -3,12 +3,12 @@ package main
 func ContainerWithMostWater(height []int) int {
 	max := 0
 
-	for i := 0; i < len(height); i++ {
+	for i := 0; i < len(height)-1; i++ {
 		if (len(height)-i-1)*height[i] <= max {
 			continue
 		}
 		for j := i + 1; j < len(height); j++ {
-			temp := Diff(i, j) * Min(height[i], height[j])
+			temp := (j - i) * Min(height[i], height[j])
 			if temp > max {
 				max = temp
 			}
@@ -22,11 +22,4 @@ func Min(x, y int) int {
 		return x
 	}
 	return y
-}
-
-func Diff(a, b int) int {
-	if a < b {
-		return b - a
-	}
-	return a - b
 }
