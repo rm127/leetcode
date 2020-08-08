8f489e1b3420a2481bc7ecfba9bf8f59c63f19e0
Rasmus <rasmus@mikkelstrup.dk>
2020-08-08 20:17:27 +0200

    Working method 💪🏻

--- 42-TrappingRainWater.go
+++ 42-TrappingRainWater.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func TrappingRainWater(height []int) int {
 	if len(height) == 0 {
 		return 0
@@ -10,21 +8,35 @@ func TrappingRainWater(height []int) int {
 	trapped := 0
 
 	for i := 0; i < len(height); i++ {
-		fmt.Println(i, "trapped:", trapped)
 		tempTrapped := 0
-		for j := i + 1; j < len(height); j++ {
-			fmt.Println("--> expanding: ", i, j, "temp: ", tempTrapped)
-			if height[j] >= height[i] {
-				fmt.Println("# reached end")
-				trapped += tempTrapped
-				i = j - 1
-				break
+		origHeight := height[i]
+		done := false
+		// loop for column height
+		for !done {
+			tempTrapped = 0
+			// increase in size to the right until column of
+			// same height or higher is reached
+			for j := i + 1; j < len(height); j++ {
+				// if same height or heigher is reached = done
+				if height[j] >= origHeight {
+					trapped += tempTrapped
+					// set index to end of dip
+					i = j - 1
+					done = true
+					break
+				} else {
+					tempTrapped += origHeight - height[j]
+				}
+			}
+			// as long as the columns temp height isn't at the
+			//// bottom => decrease in height and run again
+			if origHeight > 0 {
+				origHeight--
 			} else {
-				tempTrapped += height[i] - height[j]
+				done = true
 			}
 		}
 	}
-
 	return trapped
 }
 
