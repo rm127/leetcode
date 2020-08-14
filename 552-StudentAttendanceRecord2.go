be21e83e479951b80fb650490064008d303a9479
Rasmus <rasmus@mikkelstrup.dk>
2020-08-14 21:29:39 +0200

    getAs almost works

--- 552-StudentAttendanceRecord2.go
+++ 552-StudentAttendanceRecord2.go
@@ -6,21 +6,11 @@ import (
 )
 
 func checkRecord2(n int) int {
-	prevAdd := 0
-	fmt.Println(getAs(n))
-	return checkHelper(n, &prevAdd)
-}
-
-func checkHelper(n int, prevAdd *int) int {
-	if n == 1 {
-		return 3
+	fmt.Println("gA: ", getAs(n))
+	if n == 0 {
+		return 0
 	}
-	val := checkHelper(n-1, prevAdd) * 2
-	if n == 2 {
-		*prevAdd = 2
-	}
-	*prevAdd += n - 2
-	return val + *prevAdd
+	return int(math.Pow(3, float64(n))) - getLs(n) - getAs(n)
 }
 
 func getLs(n int) int {
@@ -31,16 +21,14 @@ func getLs(n int) int {
 }
 
 func getAs(n int) int {
-	if n > 1 {
-		prev := getAs(n - 1)
-		prevCenter := int(math.Min(float64(prev+4), 9))
-		if n == 2 {
-			prevCenter = 1
-		}
-		fmt.Println("p: ", prev, prevCenter)
-		return prev*2 + prevCenter*2 + prevCenter
+	if n == 1 {
+		return 1
 	}
-	return 0
+	prev := getAs(n - 1)
+	count := int(math.Pow(3, float64(n-2)))
+	prevCenter := int(math.Min(float64(prev+count*4), float64(count*9)))
+	fmt.Println("p: ", prev, count)
+	return prev*2 + prevCenter
 }
 
 /*
@@ -50,22 +38,30 @@ n   recs   count   LL+s   A+s     2*+x   diff   diff
 1    3       3      0      0       0      0     0
 2    9       8      0      1       2      2     2
 3    27      19     1      7       3      1     3
-4    81      43     8      33      5      2     5
-5    243     94     18     131     8      3     10
-6    729     200    45     484     12     4     12
-7    2187    418    135    1634    18     6     18
-8    6561    861    405    5295    25     7     25
-9    19683   1753   1215   16715   31     6     31
-10   59049   3536   3645   51868   30     -1    30
+4    81      43     9      33      5      2     5
+5    243     94     51     131     8      3     10
+6    729     200    ?      484     12     4     12
+7    2187    418    ?      1634    18     6     18
+8    6561    861    ?      5295    25     7     25
+9    19683   1753   ?      16715   31     6     31
+10   59049   3536   ?      51868   30     -1    30
 
 ---
 
-getAs(3) -> getAs(2) -> getAs(1)
-.prev*2+prev+4  1			 0
+27/3 = 9
+
+9 = L, 3 = L, 1 = L
+       3 = A, 1 = A
+
+9 = A, 3 = L, 1 = A
+	   3 = A
+	   3 = P, 1 = A
+9 = P, 3 = A, 1 = A
 
 ---
 
 
+A:
 .       1
 .       5
 .       1
@@ -78,9 +74,9 @@ getAs(3) -> getAs(2) -> getAs(1)
 .     1 5
 .     5 9
 .     1 5
-. . 1 5 9
+.   1 5 9
 . 1 5 9 9
-. . 1 5 9
+.   1 5 9
 .     1 5
 .     5 9
 .     1 5
@@ -93,6 +89,23 @@ getAs(3) -> getAs(2) -> getAs(1)
 .       1
 .       5
 .       1
+
+
+L:
+.
+.
+.       33
+.       9
+.       9
+.   1 7 7
+. . . 1 1
+.   . 1 1
+.       7
+.       1
+.       1
+.
+.
+.
 
 
 
@@ -111,7 +124,7 @@ PP
 
 #3
 
-= LLL
+# LLL
 LLP
 LLA
 LAL
@@ -162,7 +175,7 @@ LALP
 LAPL
 - LAPA
 LAPP
-LPLL
+# LPLL
 LPLA
 LPLP
 LPAL
@@ -200,8 +213,6 @@ APPL
 - APPA
 APPP
 
-5+9+5
-
 P
 P
 P ... same as L
@@ -237,16 +248,16 @@ LLPAP
 LLPPA
 LLPPP
 
-LALLL
-- LALLA
-LALLP
-- LALAL
+# LALLL
+#- LALLA
+#LALLP
+#- LALAL
 - LALAA
 - LALAP
-LALPL
+# LALPL
 - LALPA
 LALPP
-- LAALL
+#- LAALL
 - LAALA
 - LAALP
 - LAAAL
@@ -255,7 +266,7 @@ LALPP
 - LAAPL
 - LAAPA
 - LAAPP
-LAPLL
+# LAPLL
 - LAPLA
 LAPLP
 - LAPAL
@@ -265,45 +276,44 @@ LAPPL
 - LAPPA
 LAPPP
 
+# LPLLL
+# LPLLA
+# LPLLP
+# LPLAL
+- LPLAA
+LPLAP
+# LPLPL
+LPLPA
+LPLPP
+# LPALL
+- LPALA
+LPALP
+- LPAAL
+- LPAAA
+- LPAAP
+LPAPL
+- LPAPA
+LPAPP
+# LPPLL
+LPPLA
+LPPLP
+LPPAL
+- LPPAA
+LPPAP
+LPPPL
+LPPPA
+LPPPP
 
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-LLP
-
-ALLLL
-- ALLLA
-ALLLP
-- ALLAL
+# ALLLL
+#- ALLLA
+# ALLLP
+#- ALLAL
 - ALLAA
 - ALLAP
-ALLPL
+# ALLPL
 - ALLPA
 ALLPP
-- ALALL
+#- ALALL
 - ALALA
 - ALALP
 - ALAAL
@@ -312,7 +322,7 @@ ALLPP
 - ALAPL
 - ALAPA
 - ALAPP
-ALPLL
+# ALPLL
 - ALPLA
 ALPLP
 - ALPAL
@@ -322,7 +332,7 @@ ALPPL
 - ALPPA
 ALPPP
 
-- AALLL
+#- AALLL
 - AALLA
 - AALLP
 - AALAL
@@ -350,4 +360,121 @@ ALPPP
 - AAPPA
 - AAPPP
 
+APLLL
+- APLLA
+ALPLP
+- APALL
+- APLPA
+- APLAP
+APLPL
+- APLPA
+APLPP
+- APALL
+- APALA
+APALP
+- APAAL
+- APAAA
+- APAAP
+- APAPL
+- APAPA
+- APAPP
+APPLL
+- APPLA
+APPLP
+- APPAL
+- APPAA
+- APPAP
+APPPL
+- APPPA
+APPPP
+
+# PLLLL
+# PLLLA
+# PLLLP
+# PLLAL
+- PLLAA
+PLLAP
+# PLLPL
+PLLPA
+PLLPP
+# PLALL
+- PLALA
+PLALP
+- PLAAL
+- PLAAA
+- PLAAP
+PLAPL
+- PLAPA
+PLAPP
+# PLPLL
+PLPLA
+PLPLP
+PLPAL
+- PLPAA
+PLPAP
+PLPPL
+PLPPA
+PLPPP
+
+# PALLL
+- PALLA
+PALLP
+- PALAL
+- PALAA
+- PALAP
+PALPL
+- PALPA
+PALPP
+- PAALL
+- PAALA
+- PAALP
+- PAAAL
+- PAAAA
+- PAAAP
+- PAAPL
+- PAAPA
+- PAAPP
+PAPLL
+- PAPLA
+PAPLP
+- PAPAL
+- PAPAA
+- PAPAP
+PAPPL
+- PAPPA
+PAPPP
+
+PPLLL
+PPLLA
+PPLLP
+PPLAL
+- PPLAA
+PPLAP
+PPLPL
+PPLPA
+PPLPP
+PPALL
+- PPALA
+PPALP
+- PPAAL
+- PPAAA
+- PPAAP
+PPAPL
+- PPAPA
+PPAPP
+PPPLL
+PPPLA
+PPPLP
+PPPAL
+- PPPAA
+PPPAP
+PPPPL
+PPPPA
+PPPPP
+
+A = 131
+
+L=33
+A=9
+P=9
 */
