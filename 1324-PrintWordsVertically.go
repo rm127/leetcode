a4b42f4791e759b68dc75c51f4599f6fd9f75efb
Rasmus <rasmus@mikkelstrup.dk>
2020-08-17 14:09:19 +0200

    Reverse method. Not working, since the last letter isn't always at the end of the last string.

--- 1324-PrintWordsVertically.go
+++ 1324-PrintWordsVertically.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func printVertically(s string) []string {
 	if s == "" {
 		return []string{}
@@ -29,39 +27,28 @@ func printVertically(s string) []string {
 	// create said array
 	res := make([]string, size)
 
-	rowIndex := 0
+	rowIndex := size - 1
 	curWordSize := 0
-	passedMaxWords := 0
-	for i := 0; i < len(s); i++ {
+	// passedMaxWords := 0
+	for i := len(s) - 1; i >= 0; i-- {
 		letter := s[i]
 
 		// if the letter is a space, the current word is done
 		if letter == ' ' {
-			// if the current word is shorter than the longest
-			// and there are more maxLength words
-			// if no more maxLength words are left, skip adding (trailing) spaces
-			fmt.Println("space here", curWordSize, size, passedMaxWords, maxWords)
-			if curWordSize < size && passedMaxWords < maxWords {
-				// fill diff with spaces
-				for rowIndex < size {
-					res[rowIndex] += " "
-					rowIndex++
+			if curWordSize < size {
+				for rowIndex > 0 {
+					res[rowIndex] = " " + res[rowIndex]
+					rowIndex--
 				}
-			} else {
-				passedMaxWords++
 			}
-			// reset row and word size
 			curWordSize = 0
-			rowIndex = 0
 		} else {
 			curWordSize++
-			// add current letter to res
-			res[rowIndex] += string(letter)
-			// loop incr
-			if rowIndex == size-1 {
-				rowIndex = 0
+			res[rowIndex] = string(letter) + res[rowIndex]
+			if rowIndex == 0 {
+				rowIndex = size - 1
 			} else {
-				rowIndex++
+				rowIndex--
 			}
 		}
 	}
